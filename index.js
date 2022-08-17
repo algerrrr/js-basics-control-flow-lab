0500481198c7d2f9dd41caabd7fcb3cc0e88a357
Algernon Soriano <alger.soriano@gmail.com>
2022-08-17 20:45:51 +1000

    control_flow

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let distance = 0
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  
+  if (distance <= 400) {
+      return 'This one is on me!'
+  } else if ( distance > 2000 && distance < 2500 ) {
+      return 'I will gladly take your thirty bucks.'
+  } else  if ( distance > 2500 ) {
+      return 'No can do.'
+  }
+  
 }
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2500);
 
-function switchOnCharmFromTip(){
+let city = ""
+
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
+ternaryCheckCity("NYC");
+
+let tip = ""
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if (tip === 'generous') {
+    return 'Thank you so much.'
+  } else if (tip === 'not as generous') {
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
+}
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous')
+switchOnCharmFromTip("bad")
